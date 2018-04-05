d1b3543b936402b029e32d09b27faf2e395a46e4
aRTiPee <romantristanpol@gmail.com>
2018-04-05 20:05:43 +0800

    level 6 functionality done

---
+++ Assets/Level6Red.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Level6Red : MonoBehaviour {
+
+    public Material[] material;
+    Renderer rend;
+
+    // Use this for initialization
+    void Start () {
+        rend = gameObject.GetComponent<Renderer>();
+        rend.enabled = true;
+        rend.sharedMaterial = material[0];
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            rend.sharedMaterial = material[1];
+            Invoke("RestartGame", 1);
+        }
+    }
+
+    void RestartGame()
+    {
+        SceneManager.LoadScene(5);
+    }
+}
