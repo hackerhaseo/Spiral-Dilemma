c5284e496a7f46a1dcd8af6b251fc64d4dca6cc5
aRTiPee <romantristanpol@gmail.com>
2018-04-06 01:00:32 +0800

    level 10 functionality done

---
+++ Assets/level10red.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class level10red : MonoBehaviour {
+
+    public Material[] material;
+    Renderer rend;
+
+    // Use this for initialization
+    void Start()
+    {
+        rend = gameObject.GetComponent<Renderer>();
+        rend.enabled = true;
+        rend.sharedMaterial = material[0];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            rend.sharedMaterial = material[1];
+            Invoke("RestartGame", 1);
+        }
+    }
+
+    void RestartGame()
+    {
+        SceneManager.LoadScene(9);
+    }
+}
---
+++ Assets/level10switch.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class level10switch : MonoBehaviour {
+
+    public Material[] material;
+    Renderer rend;
+
+    public GameObject Gate1;
+
+    // Use this for initialization
+    void Start () {
+        rend = gameObject.GetComponent<Renderer>();
+        rend.enabled = true;
+        rend.sharedMaterial = material[0];
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnCollisionStay(Collision collision)
+    {
+
+        if (collision.gameObject.tag == "crate")
+        {
+            rend.sharedMaterial = material[1];
+            Gate1.transform.position = new Vector3(5.97f, 12.82f, 15.31f);
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.tag == "crate")
+        {
+            rend.sharedMaterial = material[0];
+            Gate1.transform.position = new Vector3(17.66f, 12.6f, 11.34f);
+        }
+    }
+}
